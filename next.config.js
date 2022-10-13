55c2c24d24e847cf3cae910d75e0cb205cc1c240
Ajemark <83100392+Ajemark@users.noreply.github.com>
2022-10-13 22:04:00 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -66,7 +66,7 @@ module.exports = withBundleAnalyzer({
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: securityHeaders,
       },
     ]
