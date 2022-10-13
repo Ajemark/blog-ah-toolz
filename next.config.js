fc8235f1986b65cbd03c7474f716d8faabbb7217
Ajemark <83100392+Ajemark@users.noreply.github.com>
2022-10-13 21:12:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
     key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
   },
   {
     key: 'Access-Control-Allow-Origin',
