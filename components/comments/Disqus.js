2b8dfd3fbf876f0404c61f705e9e99ffd9faed8c
Ajemark <83100392+Ajemark@users.noreply.github.com>
2022-10-14 05:29:13 +0100

    Update Disqus.js

--- components/comments/Disqus.js
+++ components/comments/Disqus.js
@@ -17,8 +17,7 @@ const Disqus = ({ frontMatter }) => {
     if (window.DISQUS === undefined) {
       const script = document.createElement('script')
       script.src = 'https://' + siteMetadata.comment.disqusConfig.shortname + '.disqus.com/embed.js'
-      script.setAttribute('data-timestamp', +new Date())
-      script.setAttribute('crossorigin', 'anonymous')
+      script.setAttribute('data-timestamp', +new Date()) 
       script.async = true
       document.body.appendChild(script)
     } else {
