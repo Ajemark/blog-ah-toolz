4701bc05aeca5fdbe4713597d09969de0e50a769
Ajemark <83100392+Ajemark@users.noreply.github.com>
2022-10-14 05:11:29 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy =
   `default-src 'self'; 
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.disqus.com/embed.js;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://blog-ah-toolz-com.disqus.com/embed.js;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src *.disqus.com/embed.js`
+  frame-src https://blog-ah-toolz-com.disqus.com/embed.js`
 
 const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
