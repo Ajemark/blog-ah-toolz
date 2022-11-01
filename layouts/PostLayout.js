2293ae85796e6ee4f48112974848331af43d357e
Ajemark <83100392+Ajemark@users.noreply.github.com>
2022-11-01 09:52:46 +0100

    Update PostLayout.js

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -8,6 +8,8 @@ import siteMetadata from '@/data/siteMetadata'
 import Comments from '@/components/comments'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 
+import NewsletterForm from '@/components/NewsletterForm'
+
 const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`
 const discussUrl = (slug) =>
   `https://mobile.twitter.com/search?q=${encodeURIComponent(
